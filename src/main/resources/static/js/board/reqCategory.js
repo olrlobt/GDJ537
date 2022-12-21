7326cc4567e0ffe9baf9b63791d6b1778157cf8e
yez-try <skeosk424@gmail.com>
2022-12-21 12:18:17 +0900

    써머노트 height추가

--- src/main/resources/static/js/board/reqCategory.js
+++ src/main/resources/static/js/board/reqCategory.js
@@ -44,7 +44,7 @@ $("#formCate").on("change",function(){
              {
                   placeholder: '내용을 입력해주세요',
                   tabsize: 2,
-                  height: 200
+                  height: 500
               });
        
 });
